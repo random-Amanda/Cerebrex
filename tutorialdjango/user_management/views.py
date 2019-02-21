31983a54757023c216d3080db0ab75afa681437f
Joseph Fernando <47097277+JRSFernando@users.noreply.github.com>
2019-02-21 22:28:11 +0530

    Add files via upload

---
+++ tutorialdjango/user_management/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
+
+import math
+sleep_data=("yolo")
+print(sleep_data)
+
+
+
+
+def home(request):
+    return HttpResponse("hello wrold") 
+    
